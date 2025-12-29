c6808bea411c9ffa87de344dc56b55473065a97d
AkaakuHub <akaaku2021media01@gmail.com>
2025-12-29 23:27:59 +0900

    feat: step 3

--- Assets/Scripts/BasicSpawner.cs
+++ Assets/Scripts/BasicSpawner.cs
@@ -42,6 +42,13 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
         }
     }
 
+    private bool _mouseButton0;
+
+    private void Update()
+    {
+        _mouseButton0 = _mouseButton0 | Input.GetMouseButton(0);
+    }
+
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
         var data = new NetworkInputData();
@@ -57,6 +64,9 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 
         if (Input.GetKey(KeyCode.D))
             data.direction += Vector3.right;
+
+        data.buttons.Set(NetworkInputData.MOUSEBUTTON0, _mouseButton0);
+        _mouseButton0 = false;
 
         input.Set(data);
     }
